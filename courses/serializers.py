c22a712e778c4e37534eb1aeedcd0267e81d0959
SchawnnDev <iperskill@gmail.com>
2021-10-15 22:20:19 +0200

    exam dates added to semester

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
 
-from courses.models import Course, Note, TpGroup, TdGroup, Semester, ExamDate
+from courses.models import Course, Note, TpGroup, TdGroup, Semester, ExamDate, Localisation, LocalisationImage
+
+
+class ExamDateSerializer(serializers.ModelSerializer):
+    start = serializers.DateTimeField()
+    end = serializers.DateTimeField()
+    tp_group = serializers.CharField(source='tp_group.id', allow_null=True)
+    localisation = serializers.IntegerField(source='localisation.id', allow_null=True)
+    note = serializers.CharField(source='note.id')
+
+    class Meta:
+        model = ExamDate
+        fields = ('start', 'end', 'tp_group', 'localisation', 'note')
 
 
 class NoteSerializer(serializers.ModelSerializer):
@@ -9,10 +21,11 @@ class NoteSerializer(serializers.ModelSerializer):
     coeff = serializers.FloatField()
     denominator = serializers.IntegerField()
     weight = serializers.FloatField()
+    exam_dates = ExamDateSerializer(many=True, source='date_notes')
 
     class Meta:
         model = Note
-        fields = ('id', 'name', 'coeff', 'denominator', 'weight', 'notes')
+        fields = ('id', 'name', 'coeff', 'denominator', 'weight', 'exam_dates', 'notes')
 
     def get_fields(self):
         fields = super(NoteSerializer, self).get_fields()
@@ -37,15 +50,6 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'ects', 'prof', 'weight', 'color', 'dark_color', 'optional', 'notes')
 
 
-class ExamDateSerializer(serializers.ModelSerializer):
-    date = serializers.DateTimeField()
-    note = serializers.CharField(source='note.id')
-
-    class Meta:
-        model = ExamDate
-        fields = ('date', 'note')
-
-
 class TpGroupSerializer(serializers.ModelSerializer):
     number = serializers.IntegerField()
 
@@ -68,7 +72,45 @@ class SemesterSerializer(serializers.ModelSerializer):
     activated = serializers.BooleanField()
     courses = CourseSerializer(many=True, source='course_semesters')
     td_groups = TdGroupSerializer(many=True, source='semesters')
+    exam_dates = serializers.SerializerMethodField()
 
     class Meta:
         model = Semester
-        fields = ('number', 'activated', 'courses', 'td_groups')
+        fields = ('number', 'activated', 'courses', 'td_groups', 'exam_dates')
+
+    def get_exam_dates(self, obj):
+        if not hasattr(obj, 'id') or obj.id is None:
+            return []
+        result = []
+        exam_dates = ExamDate.objects.filter(note__course__semester=obj.id)
+        for exam_date in exam_dates:
+            result.append(ExamDateSerializer(exam_date).data)
+        return result
+
+
+class LocalisationSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    x = serializers.FloatField()
+    y = serializers.FloatField()
+    image = serializers.IntegerField(source='image.id')
+
+    class Meta:
+        model = Localisation
+        fields = ('id', 'name', 'x', 'y', 'image')
+
+
+class LocalisationImageSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    path = serializers.CharField()
+    description = serializers.CharField()
+
+    class Meta:
+        model = LocalisationImage
+        fields = ('id', 'path', 'description')
+
+
+class DataSerializer(serializers.Serializer):
+    localisations = LocalisationSerializer(many=True)
+    localisation_images = LocalisationImageSerializer(many=True)
+    semesters = SemesterSerializer(many=True)
--- courses/views.py
+++ courses/views.py
@@ -3,17 +3,18 @@ from rest_framework import mixins, viewsets
 from rest_framework.response import Response
 from rest_framework.throttling import AnonRateThrottle
 
-from courses.models import Semester
-from courses.serializers import SemesterSerializer
+from courses.models import Semester, Localisation, LocalisationImage, ExamDate
+from courses.serializers import DataSerializer
 
 
 class SemesterViewSet(mixins.ListModelMixin,
                       viewsets.GenericViewSet):
-    serializer_class = SemesterSerializer
-    queryset = Semester.objects
     throttle_classes = [AnonRateThrottle]
 
     def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
+        data = {'semesters': Semester.objects.all(),
+                'localisations': Localisation.objects.all(),
+                'localisation_images': LocalisationImage.objects.all()
+                }
+        serializer = DataSerializer(data)
         return Response(serializer.data)
