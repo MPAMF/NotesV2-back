f4e944d0000d7a27764938ab4b0461dfe16ff678
SchawnnDev <iperskill@gmail.com>
2022-10-13 01:41:52 +0200

    added sorting to note admin

--- courses/admin.py
+++ courses/admin.py
@@ -9,10 +9,14 @@ class CourseAdmin(admin.ModelAdmin):
     ordering = ('semester',)
 
 
+class NoteAdmin(admin.ModelAdmin):
+    ordering = ('course',)
+
+
 admin.site.register(Semester)
 admin.site.register(Degree)
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Note)
+admin.site.register(Note, NoteAdmin)
 admin.site.register(Localisation)
 admin.site.register(LocalisationImage)
 admin.site.register(ExamDate)
